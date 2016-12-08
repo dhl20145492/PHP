4a198c8ffd3171af5a29a67523a2f5910cdc5e46
菜单 <b1906999678@163.com>
2016-12-08 22:00:34 +0800

    乘法表 &nbsp不明白是啥

--- day1/lx/chegnfabiao.php
+++ day1/lx/chegnfabiao.php
@@ -11,9 +11,33 @@
 //    }
 //    echo "<br />";
 //}
+
+
+
+//echo "<table width = 1000px align = center border = 1>";
+//$color = "";
+//for ($i = 1;$i<=9;$i++){
+//    if ($i%2 == 0){
+//        $color = "white";
+//    }
+//    else{
+//        $color = "grace";
+//    }
+//    echo "<tr bgColor = ".$color.">";
+//    for ($j = 1;$j<=$i;$j++){
+//        echo "<td>";
+//        echo $i."*".$j."=".($i*$j);
+//        echo "</td>";
+//    }
+//    echo "<br />";
+//    echo "</tr>";
+//}
+//echo "<table>";
+
+
 echo "<table width = 1000px align = center border = 1>";
 $color = "";
-for ($i = 1;$i<=9;$i++){
+for ($i = 9;$i>=1;$i--){
     if ($i%2 == 0){
         $color = "white";
     }
@@ -23,7 +47,7 @@ for ($i = 1;$i<=9;$i++){
     echo "<tr bgColor = ".$color.">";
     for ($j = 1;$j<=$i;$j++){
         echo "<td>";
-        echo $i."*".$j."=".($i*$j);
+        echo $i."*".$j."=".($i*$j)."&nbsp;";
         echo "</td>";
     }
     echo "<br />";
