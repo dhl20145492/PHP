141f48c091f5afc74c1a3b1151f9b5e2d6ee6ae3
菜单 <b1906999678@163.com>
2017-01-14 22:36:59 +0800

    留言板

---
+++ liuyanban/addcatelog.php
@@ -0,0 +1,12 @@
+<?php
+
+
+
+
+
+?>
+<!--<meta charset="utf-8">-->
+<!--<form action="addcatelog.php" method="post">-->
+<!--分类名:<input type="text" name="cname" >&nbsp;&nbsp;-->
+<!--    <input type="submit" name="sub" value="增加分类">-->
+<!--</form>-->
---
+++ liuyanban/conn.php
@@ -0,0 +1,11 @@
+<?php
+//了解数据库
+//数据库引擎（）
+//1.打开数据库连接
+$link = @mysqli_connect('localhost','root','root') or die('数据库连接失败');
+//2.选择数据库
+@mysqli_select_db($link,'blog1') or die('选择数据库失败');
+//3.设置传输编码
+@mysqli_set_charset($link,'utf8');
+
+?>
